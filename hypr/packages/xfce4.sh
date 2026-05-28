8d048a38e2d84a8cad8b93025e13942146be1ed3
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-05-28 14:18:52 +0100

    Update xfce4.sh

--- hypr/packages/xfce4.sh
+++ hypr/packages/xfce4.sh
@@ -3,3 +3,4 @@ figlet -f 3d "XFCE4"
 echo " XFCE4 "
 sudo pacman -Rns xfce4 xfce4-goodies parole catfish --noconfirm
 sudo pacman -S xfce4 xfce4-goodies parole catfish --noconfirm
+yay -S tumbler-extra-thumbnailers --noconfirm
