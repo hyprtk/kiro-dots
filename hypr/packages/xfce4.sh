dd0c218f778a393403ad615eaca01fa7614fa7fc
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-06-19 13:09:39 +0100

    hyprland 55 lua migration

--- hypr/packages/xfce4.sh
+++ hypr/packages/xfce4.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 figlet -f 3d "XFCE4"
 echo " XFCE4 "
-sudo pacman -Rns xfce4 xfce4-goodies parole catfish --noconfirm
-sudo pacman -S xfce4 xfce4-goodies parole catfish --noconfirm
+sudo pacman -S xfce4 xfce4-goodies parole --noconfirm
 yay -S tumbler-extra-thumbnailers --noconfirm
